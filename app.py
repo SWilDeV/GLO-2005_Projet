cdb3dbd09ce5bc06339f3dd3e44c50afb4212379
SWilDeV <sean0669@gmail.com>
2022-04-06 18:02:15 +0200

    connect db

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 #pipenv install --dev
 from flask import Flask, request, jsonify, render_template
 from flask_marshmallow import Marshmallow
+import pymysql, pymysql.cursors
 import os
 
 
 #Init app
 app = Flask(__name__)
+basedir =os.path.abspath(os.path.dirname(__file__))
+
+#Database
+connexion= pymysql.connect(host='localhost',user='root', password='sirius3',db='testdb')
 
 @app.route('/', methods=['GET'])
 def get():
