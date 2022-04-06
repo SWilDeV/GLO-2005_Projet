b7c484a43d9eab50cdc7b90ca267a850c21c1458
SWilDeV <sean0669@gmail.com>
2022-04-06 22:21:10 +0200

    DB connexion

--- app.py
+++ app.py
@@ -2,6 +2,9 @@
 from flask import Flask, request, jsonify, render_template
 from flask_marshmallow import Marshmallow
 import pymysql, pymysql.cursors
+from dotenv import load_dotenv
+load_dotenv()
+
 import os
 
 
@@ -10,7 +13,7 @@ app = Flask(__name__)
 basedir =os.path.abspath(os.path.dirname(__file__))
 
 #Database
-connexion= pymysql.connect(host='localhost',user='root', password='sirius3',db='testdb')
+connexionDB= pymysql.connect(host='localhost',user=os.environ.get('USER'), password=os.environ.get('PASSWORD'),db='ChallongeII')
 
 @app.route('/', methods=['GET'])
 def get():
