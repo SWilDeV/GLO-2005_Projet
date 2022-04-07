e6b48d11dc89a3b51fab3de395b55b250434edb4
SWilDeV <sean0669@gmail.com>
2022-04-07 14:08:39 +0200

    vue installed

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 #pipenv install --dev
 from flask import Flask, request, jsonify, render_template
-from flask_marshmallow import Marshmallow
+from flask_cors import CORS
 import pymysql, pymysql.cursors
 from dotenv import load_dotenv
 load_dotenv()
@@ -10,15 +10,43 @@ import os
 
 #Init app
 app = Flask(__name__)
+app.config.from_object(__name__)
+CORS(app, resources={r"/*":{'origins':"*"}})
 basedir =os.path.abspath(os.path.dirname(__file__))
 
 #Database
-connexionDB= pymysql.connect(host='localhost',user=os.environ.get('USER'), password=os.environ.get('PASSWORD'),db='ChallongeII')
+class Database:
+    def __init__(self):
+        host='localhost',
+        user=os.environ.get('USER'), 
+        password=os.environ.get('PASSWORD'),
+        db=os.environ.get('DB')
+        self.con = pymysql.connect(host='localhost',user=os.environ.get('USER'), password=os.environ.get('PASSWORD'),db=os.environ.get('DB'))
+        self.cur=self.con.cursor()
+
+
+    def list_Artistes(self):
+        self.cur.execute("SELECT * FROM Artistes")
+        result = self.cur.fetchall()
+        return result
 
 @app.route('/', methods=['GET'])
 def get():
-    return jsonify({'msg':'Hello World'})
+    return render_template("home.html")
+# def get():
+#     return jsonify({'msg':'Hello World'})
 
+@app.route('/Artistes', methods=['GET'])
+def getArtistes():
+    def db_query():
+        db= Database()
+        Artistes = db.list_Artistes()
+        
+        return Artistes
+
+    res= db_query()
+    print(res)
+    return render_template("artistes.html", result = res)
 #Run server
 if __name__ == '__main__':
     app.run(debug=True)
